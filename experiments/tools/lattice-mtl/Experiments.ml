4fb63b2baccc4d993c92e917835a0318539a5ec1
Agnishom <cagnishom@gmail.com>
2021-01-15 13:32:11 -0600

    started reorganizing experiments

---
+++ experiments/tools/lattice-mtl/Experiments.ml
@@ -0,0 +1,86 @@
+open LatticeMtl;;
+
+let buff = [| 0.3954132773952119 ; 0.7802043456276684 ; 0.9056838067459964 ;
+                 0.9919627616090054 ; 0.3153684746871873 ; 0.868398327264566 ;
+                 0.8470972988532026 ; 0.7257287331532113 ; 0.8272851976726383 ;
+                 0.15529920818726972 ;
+              |]
+
+let test1 = (fun x -> x -. 0.50)
+let test2 = (fun x -> x -. 0.00)
+
+let realL = {join = (max); meet = (min); }
+let realLB = {top = infinity; bottom = neg_infinity; }
+
+let fAtomic1 = FAtomic test1
+let fAtomic2 = FAtomic test2
+
+let sometime_up b = FSometime(0, b, fAtomic1)
+let sometime_at b = fDelay b fAtomic1
+let sometime_lo_up b = FSometime(b, 2*b, fAtomic1)
+let sometime_lo b = FSometimeUnbounded(b, fAtomic1)
+
+let since_up b = FSince(0, b, fAtomic1, fAtomic2)
+let since_at b = FSince(b, b, fAtomic1, fAtomic2)
+let since_lo_up b = FSince(b, 2*b, fAtomic1, fAtomic2)
+let since_lo b = FSinceUnbounded(b, fAtomic1, fAtomic2)
+
+let forms = [sometime_up; sometime_at; sometime_lo_up; sometime_lo;
+            since_up; since_at; since_lo_up; since_lo]
+
+let describer f bound =
+  if (f == 0)
+  then "once[0:" ^ string_of_int(bound) ^ "] x > 0.5"
+  else if (f == 1)
+  then "once[" ^ string_of_int(bound) ^ ":" ^ string_of_int(bound) ^ "] x > 0.5"
+  else if (f == 2)
+  then "once[" ^ string_of_int(bound) ^ ":" ^ string_of_int(2*bound) ^ "] x > 0.5"
+  else if (f == 3)
+  then "once[" ^ string_of_int(bound) ^ ":] x > 0.5"
+  else if (f == 4)
+  then "x > 0 since[0:" ^ string_of_int(bound) ^ "] x > 0.5"
+  else if (f == 5)
+  then "x > 0 since[" ^ string_of_int(bound) ^ ":" ^ string_of_int(bound) ^ "] x > 0.5"
+  else if (f == 6)
+  then "x > 0 since[" ^ string_of_int(bound) ^ ":" ^ string_of_int(2*bound) ^ "] x > 0.5"
+  else if (f == 7)
+  then "x > 0 since[" ^ string_of_int(bound) ^ ":] x > 0.5"
+  else "wrong formula argument"
+
+let measure form strmlen desc =
+  print_endline "Tool=lattice-monitor";
+  print_endline ("Formula="^desc);
+  print_endline ("StremLength="^ (string_of_int strmlen));
+  let monbox = ref (toMonitor realL realLB form) in
+  let cntbox = ref 0 in
+  let thenn = Unix.gettimeofday () in
+  for i = 0 to strmlen-1 do
+      let inp = (Array.get buff (i mod 10)) in
+      if ((mOut !monbox inp) > 0.0)
+      then begin cntbox := !cntbox + 1; end
+      else ();
+      monbox := mNext (!monbox) inp;
+  done;
+  let now = Unix.gettimeofday () in
+  print_endline ("TimeUnit = ms");
+  print_endline ("TimeElapsed = " ^ string_of_float ((now -. thenn)* 1000.0))
+
+let formarg = ref 0
+let bound = ref 0
+let streamSize = ref 0
+
+let speclist = [
+      ("-f", Arg.Set_int formarg, ": set formula");
+      ("-b", Arg.Set_int bound, ": set bound");
+      ("-l", Arg.Set_int streamSize, ": set streamSize");
+    ]
+
+let suite () =
+        measure ((List.nth forms !formarg) !bound) (!streamSize) (describer !formarg !bound)
+
+let main =
+    Arg.parse
+     speclist
+     (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
+     "-f [0-7] -b [bound] -l [strmlen]";
+    suite ()
---
+++ experiments/tools/semiring-monitor/experiments.rs
@@ -0,0 +1,130 @@
+/**
+
+<< "Please use this program the following way:" << '\n'
+      << "bin [form] [strmlen] [b1]" << '\n'
+      << "form should be 0 - 7" << '\n'
+      << "strmlen, b1 are natural numbers" << '\n'
+      << "fff == 0 means P_[0,b1]" << '\n'
+      << "fff == 1 means P_[b1,b1]" << '\n'
+      << "fff == 2 means P_[b1,2*b1]" << '\n'
+      << "fff == 3 means P_[b1,inf]" << '\n'
+      << "fff == 4 means S_[0,b1]" << '\n'
+      << "fff == 5 means S_[b1,b1]" << '\n'
+      << "fff == 6 means S_[b1,2*b1]" << '\n'
+      << "fff == 7 means S_[b1,inf]" << '\n'
+
+**/
+
+extern crate signal_monitor;
+
+use std::time::{ Instant, Duration };
+use signal_monitor::utils::*;
+use signal_monitor::utils::semiring::*;
+use signal_monitor::eval::*;
+use signal_monitor::query::*;
+use signal_monitor::sink::*;
+use signal_monitor::qmtl::*;
+
+use std::env;
+
+fn main() {
+
+    fn over_half(x: Float64) -> Float64 { x - Float64::new(1.0 / 2.0) }
+    fn over_zero(x: Float64) -> Float64 { x - Float64::new(0.0) }
+
+    let f_half = prop::atomic::<Float64,SemiringFloat64>(over_half);
+    let f_zero = prop::atomic::<Float64,SemiringFloat64>(over_zero);
+
+    let form0 = |b| past::sometime_up(b, f_half.clone());
+    let form1 = |b| past::sometime_at(b, f_half.clone());
+    let form2 = |b| past::sometime_lo_up(b, 2*b, f_half.clone());
+    let form3 = |b| past::sometime_lo(b, f_half.clone());
+
+    let form4 = |b| past::since_up(b, f_half.clone(), f_zero.clone());
+    let form5 = |b| past::since_at(b, f_half.clone(), f_zero.clone());
+    let form6 = |b| past::since_lo_up(b, 2*b, f_half.clone(), f_zero.clone());
+    let form7 = |b| past::since_lo(b, f_half.clone(), f_zero.clone());
+
+    let args: Vec<String> = env::args().collect();
+    let mut formarg : i32 = 0;
+    let mut bound : u32 = 0;
+    let mut strmlen : u32 = 0;
+
+    let describer = |f, b| {
+        match f {
+            0 => format!("once[0:{}] x > 0.5", b),
+            1 => format!("once[{}:{}] x > 0.5", b, b),
+            2 => format!("once[{}:{}] x > 0.5", b, 2*b),
+            3 => format!("once[{}:] x > 0.5", b),
+            4 => format!("x > 0.5 since[0:{}] x > 0", b),
+            5 => format!("x > 0.5 since[{}:{}] x > 0", b, b),
+            6 => format!("x > 0.5 since[{}:{}] x > 0", b, 2*b),
+            7 => format!("x > 0.5 since[{}:] x > 0", b),
+            _ => "bad formula".to_string()
+        }
+    };
+
+    // Rust does not allow arrays of lambdas? I am sad :(
+    //
+    //let forms : Vec<closure()> = vec![form0, form1, form2, form3, form4, form5, form6, form7];
+
+    match args.len(){
+        4 =>{
+            match args[1].parse(){
+                Ok(f) => formarg = f,
+                _ => println!("couldn't parse formarg"),
+            }
+            match args[2].parse(){
+                Ok(s) => strmlen = s,
+                _ => println!("couldn't parse strmlen"),
+            }
+            match args[3].parse(){
+                Ok(b) => bound = b,
+                _ => println!("couldn't parse bound"),
+            }
+        }
+        _ => println!("Please check top of source code for usage instructions")
+    }
+
+    match formarg{
+        0 => measure(form0(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        1 => measure(form1(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        2 => measure(form2(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        3 => measure(form3(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        4 => measure(form4(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        5 => measure(form5(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        6 => measure(form6(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        7 => measure(form7(bound).get_monitor(), strmlen, describer(formarg, bound)),
+        _ => println!("bad formarg"),
+    };
+
+}
+
+pub fn measure<Q:Query<Float64,Float64>>(q: Q, strmlen : u32, desc: String){
+
+    let buff = [ 0.3954132773952119 , 0.7802043456276684 , 0.9056838067459964 ,
+                     0.9919627616090054 , 0.3153684746871873 , 0.868398327264566 ,
+                     0.8470972988532026 , 0.7257287331532113 , 0.8272851976726383 ,
+                     0.15529920818726972
+                ];
+
+    println!("Tool=semiring-monitor");
+    println!("Formula={}",desc);
+    println!("StreamLength={}",strmlen);
+
+    let start: Instant = Instant::now();
+	let mut snk = SLastCount::default();
+	let mut eval = q.start(&mut snk);
+
+    for iii in 0..(strmlen-1){
+        eval.next(Float64::new(buff[(iii as usize)%10]), &mut snk);
+    }
+
+    let now: Instant = Instant::now();
+    let elapsed : Duration = now.duration_since(start);
+
+    println!("TimeUnit={}","ms");
+    println!("TimeElapsed={}", elapsed.as_millis());
+
+    eval.end(&mut snk);
+}
