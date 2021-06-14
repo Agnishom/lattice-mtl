af34c9f6422b585d1f4deac9897ef0d813692b23
Agnishom Chattopadhyay <cagnishom@gmail.com>
2021-06-14 15:47:57 -0500

    added similar proofs for since dual, since a b

--- src/Monitor/NewMonitor.v
+++ src/Monitor/NewMonitor.v
@@ -29,7 +29,22 @@ Definition monSinceUp (n : nat) : @Monitor Val (Val * Val) :=
     (monSince (monAtomic fst) (monAtomic snd))
     (monSometimeBounded n (monAtomic snd)).
 
-Lemma monSinceUp_correcntess:
+Definition monSinceAB (pa : nat) (b : nat) : @Monitor Val (Val * Val) :=
+  @monAnd Val (Val * Val) lattice_val
+          (monDelay (S pa) (monSinceUp (b - (S pa))))
+          (monAlwaysBounded pa (monAtomic fst)).
+
+Definition monSinceDualUp (n : nat) : @Monitor Val (Val * Val) :=
+  @monOr Val (Val * Val) lattice_val
+    (monSinceDual (monAtomic fst) (monAtomic snd))
+    (monAlwaysBounded n (monAtomic snd)).
+
+Definition monSinceDualAB (pa : nat) (b : nat) : @Monitor Val (Val * Val) :=
+  @monOr Val (Val * Val) lattice_val
+          (monDelayDual (S pa) (monSinceDualUp (b - (S pa))))
+          (monSometimeBounded pa (monAtomic fst)).
+
+Lemma monSinceUp_correctness:
   forall n, implements (monSinceUp n) (FSince 0 n (FAtomic fst) (FAtomic snd)).
 Proof.
   unfold implements. intros.
@@ -40,6 +55,63 @@ Proof.
   - apply monSince_correctness. auto.
     apply monAtomic_correctness.
     apply monAtomic_correctness.
+  - apply monSometimeBounded_correctness.
+    apply monAtomic_correctness.
+Qed.
+
+Lemma monSinceAB_correctness:
+  forall pa b, pa < b
+          -> implements (monSinceAB pa b) (FSince (S pa) b (FAtomic fst) (FAtomic snd)).
+Proof.
+  unfold implements. intros.
+  remember (FSince _ _ _ _) as f.
+  rewrite -> robustness_eq with (ψ := normalize f) by now apply normalize_correctness.
+  subst. simpl. apply <- Nat.ltb_lt in H. rewrite H.
+  replace ((FAnd (FSinceUnbounded 0 (FAtomic fst) (FAtomic snd)) (FSometime 0 (b - S pa) (FAtomic snd))))
+    with (normalize (FSince 0 (b -  S pa) (FAtomic fst) (FAtomic snd))) by auto.
+  remember (FSince 0 (b -  S pa) (FAtomic fst) (FAtomic snd)) as g.
+  unfold monSinceAB.
+  apply monAnd_correctness.
+  - apply monDelay_correctness.
+    -- unfold implements. intros. rewrite monSinceUp_correctness.
+       apply robustness_eq. intros. rewrite <- normalize_correctness.
+       subst. auto. auto.
+  - apply monAlwaysBounded_correctness.
+    apply monAtomic_correctness.
+Qed.
+
+Lemma monSinceDualUp_correctness:
+  forall n, implements (monSinceDualUp n) (FSinceDual 0 n (FAtomic fst) (FAtomic snd)).
+Proof.
+  unfold implements. intros.
+  remember (FSinceDual _ _ _ _) as f.
+  rewrite -> robustness_eq with (ψ := normalize f) by now apply normalize_correctness.
+  subst. simpl. unfold monSinceDualUp.
+  apply monOr_correctness.
+  - apply monSinceDual_correctness. auto.
+    apply monAtomic_correctness.
+    apply monAtomic_correctness.
+  - apply monAlwaysBounded_correctness.
+    apply monAtomic_correctness.
+Qed.
+
+Lemma monSinceDualAB_correctness:
+  forall pa b, pa < b
+          -> implements (monSinceDualAB pa b) (FSinceDual (S pa) b (FAtomic fst) (FAtomic snd)).
+Proof.
+  unfold implements. intros.
+  remember (FSinceDual _ _ _ _) as f.
+  rewrite -> robustness_eq with (ψ := normalize f) by now apply normalize_correctness.
+  subst. simpl. apply <- Nat.ltb_lt in H. rewrite H.
+  replace ((FOr (FSinceDualUnbounded 0 (FAtomic fst) (FAtomic snd)) (FAlways 0 (b - S pa) (FAtomic snd))))
+    with (normalize (FSinceDual 0 (b -  S pa) (FAtomic fst) (FAtomic snd))) by auto.
+  remember (FSinceDual 0 (b -  S pa) (FAtomic fst) (FAtomic snd)) as g.
+  unfold monSinceDualAB.
+  apply monOr_correctness.
+  - apply monDelayDual_correctness.
+    -- unfold implements. intros. rewrite monSinceDualUp_correctness.
+       apply robustness_eq. intros. rewrite <- normalize_correctness.
+       subst. auto. auto.
   - apply monSometimeBounded_correctness.
     apply monAtomic_correctness.
 Qed.
@@ -259,7 +331,7 @@ Proof.
   unfold implements.
   intros.
   rewrite mPar_mCompose.
-  rewrite monSinceUp_correcntess.
+  rewrite monSinceUp_correctness.
   unfold robustness. simpl.
   rewrite length_toList.
   rewrite -> neZip_neLength
@@ -272,6 +344,111 @@ Proof.
   intros.
   rewrite -> (extend_gCollect Val A) with (ϕ0 := ψ).
   under meet_i_ext_in => k.
+  intros. rewrite -> fst_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ϕ).
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  auto.
+Qed.
+
+Lemma monSinceAB_composition (m n : Monitor) (ϕ ψ : Formula A):
+  implements m ϕ
+  -> implements n ψ
+  -> forall pa b,
+      pa < b
+      -> implements ((mPar m n) >> (monSinceAB pa b))
+                   (FSince (S pa) b ϕ ψ).
+Proof.
+  unfold implements.
+  intros.
+  rewrite mPar_mCompose.
+  rewrite monSinceAB_correctness.
+  auto. unfold robustness. simpl.
+  rewrite length_toList.
+  rewrite -> neZip_neLength
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite gCollect_neLength.
+  rewrite length_toList.
+  under join_b_ext_in => j.
+  rewrite -> snd_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  intros.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ψ).
+  under meet_i_ext_in => k.
+  intros. rewrite -> fst_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ϕ).
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  auto.
+Qed.
+
+Lemma monSinceDualUp_composition (m n : Monitor) (ϕ ψ : Formula A):
+  implements m ϕ
+  -> implements n ψ
+  -> forall a, implements ((mPar m n) >> monSinceDualUp a) (FSinceDual 0 a ϕ ψ).
+Proof.
+  unfold implements.
+  intros.
+  rewrite mPar_mCompose.
+  rewrite monSinceDualUp_correctness.
+  unfold robustness. simpl.
+  rewrite length_toList.
+  rewrite -> neZip_neLength
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite gCollect_neLength.
+  rewrite length_toList.
+  under meet_b_ext_in => j.
+  rewrite -> snd_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  intros.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ψ).
+  under join_i_ext_in => k.
+  intros. rewrite -> fst_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ϕ).
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  over.
+  auto.
+  rewrite <- length_toList. pose proof (length_toList1 σ). lia.
+  auto.
+Qed.
+
+Lemma monSinceDualAB_composition (m n : Monitor) (ϕ ψ : Formula A):
+  implements m ϕ
+  -> implements n ψ
+  -> forall pa b,
+      pa < b
+      -> implements ((mPar m n) >> (monSinceDualAB pa b))
+                   (FSinceDual (S pa) b ϕ ψ).
+Proof.
+  unfold implements.
+  intros.
+  rewrite mPar_mCompose.
+  rewrite monSinceDualAB_correctness.
+  auto. unfold robustness. simpl.
+  rewrite length_toList.
+  rewrite -> neZip_neLength
+    by now repeat rewrite gCollect_neLength; auto.
+  rewrite gCollect_neLength.
+  rewrite length_toList.
+  under meet_b_ext_in => j.
+  rewrite -> snd_extend_neZip
+    by now repeat rewrite gCollect_neLength; auto.
+  intros.
+  rewrite -> (extend_gCollect Val A) with (ϕ0 := ψ).
+  under join_i_ext_in => k.
   intros. rewrite -> fst_extend_neZip
     by now repeat rewrite gCollect_neLength; auto.
   rewrite -> (extend_gCollect Val A) with (ϕ0 := ϕ).
